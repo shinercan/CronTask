5cccb84a1ea9aa7408678ca5ca6abee36dcc337c
shinercan <wenneng.zhu@qq.com>
2018-07-19 13:20:06 +0800

    T4摸版更新

---
+++ CronTask.Entities/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronTask.Entities
+{
+    public class Configuration
+    {
+        public object ConConectionString { get; set; }
+    }
+}
