386df469ed023aaa80c27babea950290f98fefa3
shinercan <wenneng.zhu@qq.com>
2018-07-19 09:15:59 +0800

    添加生成的实体

---
+++ CronTask.Entities/DbFactory.cs
@@ -0,0 +1,28 @@
+﻿using PetaPoco.NetCore;
+using Pomelo.Data.MySql;
+using System;
+using System.Threading;
+
+namespace CronTask.Entities
+{
+    public class DbFactory : IDisposable
+    {
+        private static AsyncLocal<Database> dataContext = new AsyncLocal<Database>();
+        public static Database Default
+        {
+            get
+            {
+                if (dataContext.Value == null)
+                {
+                    MySqlConnection connection = new MySqlConnection("server=120.79.84.18;database=himall_test;uid=himall_admin;password=himall@yw;");
+                    dataContext.Value = new Database(connection);
+                }
+                return dataContext.Value;
+            }
+        }
+        public void Dispose()
+        {
+            dataContext = null;
+        }
+    }
+}
